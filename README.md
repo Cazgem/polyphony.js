146602b10e58e082ce8becd95cefc90f7cab9094
Cazgem <cazgem@gmail.com>
2020-11-12 05:22:20 +0000

    Init Public Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#polyphony.js
+
+Back-end for My Twitch Bot, <a href="https://github.com/polyphonyTwitch">Polyphony</a>
+
+See the bot in action at <a href="https://twitch.tv/cazgem">My Twitch</a>
+
+<a href="https://twitch.tv/polyphony">Get It Yourself!</a>
---
+++ index.js
@@ -0,0 +1,14 @@
+//polyphony.js
+const request = require(`request`);
+const mysql = require(`mysql`);
+const Twitch = require(`./lib/twitch`);
+const OBS = require(`./lib/obs`);
+const Twitter = require(`./lib/twitter`);
+class Polyphony {
+    constructor(config, port) {
+        this.Twitch = new Twitch(config, port);
+        this.OBS = new OBS(config);
+        this.Twitter = new Twitter(config);
+    }
+}
+module.exports = Polyphony;
