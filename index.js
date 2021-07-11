3bdd7f809afb19ffaadf585d96a88fe7d41ff184
Cazgem <cazgem@gmail.com>
2021-07-11 03:48:25 +0000

    bearer_token

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const Twitch = require(`./lib/twitch`);
 const OBS = require(`./lib/obs`);
 const Twitter = require(`./lib/twitter`);
+const Quaver = require(`quaver`);
 class Polyphony {
     constructor(config, port) {
         this.Twitch = new Twitch(config, port);
         this.OBS = new OBS(config);
         this.Twitter = new Twitter(config);
+        this.Symphony = new Quaver(config);
     }
 }
 module.exports = Polyphony;
