77644b351b3f2bd018ac54da0074b40175c25994
Cazgem <cazgem@gmail.com>
2021-07-11 07:59:16 +0000

    Promise Bugfix

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const Twitch = require(`./lib/twitch`);
 const OBS = require(`./lib/obs`);
 const Twitter = require(`./lib/twitter`);
-const Quaver = require(`quaver`);
+const Symphony = require(`@cazgem/symphony.js`);
 class Polyphony {
     constructor(config, port) {
         this.Twitch = new Twitch(config, port);
         this.OBS = new OBS(config);
         this.Twitter = new Twitter(config);
-        this.Symphony = new Quaver(config);
+        this.Symphony = new Symphony(config);
     }
 }
 module.exports = Polyphony;
