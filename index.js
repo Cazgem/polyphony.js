b93b7dc659c593302169638729a405c0bf5cb6ca
Cazgem <cazgem@gmail.com>
2021-07-25 02:51:55 +0000

    removed OBS due to bug

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //polyphony.js
 const Twitch = require(`./lib/twitch`);
-const OBS = require(`./lib/obs`);
+// const OBS = require(`./lib/obs`);
 const Twitter = require(`./lib/twitter`);
 const Symphony = require(`@cazgem/symphony.js`);
 class Polyphony {
     constructor(config, port) {
         this.Twitch = new Twitch(config, port);
-        this.OBS = new OBS(config);
+        // this.OBS = new OBS(config);
         this.Twitter = new Twitter(config);
         this.Symphony = new Symphony(config);
     }
